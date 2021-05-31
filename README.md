73cd566343e1cf12ab3643aaa4d1d632cbc0c1e6
diegosbu <diegos@bu.edu>
2021-05-30 21:57:57 -0400

    Set up homepage and team logos added

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1>NBA Tracker</h1>
+
+This web app is a personal project. Users are able to view relevant stats for all NBA players/teams. It also allows users to create a list of their favorite NBA players/teams.
---
+++ app.js
@@ -0,0 +1,31 @@
+// Module dependencies
+
+const ejs = require("ejs");
+const express = require("express");
+const path = require("path");
+const app = express();
+
+// Config
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+// Middleware 
+
+app.use("/public", express.static("public"));
+
+// Routes
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+app.get("/teams", (req, res) => {
+    res.render("teams")
+})
+
+// Server
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
