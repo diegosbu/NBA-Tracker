e4801bc0c02b33f9e9f252e67e62a9d378d5b15a
diegosbu <diegos@bu.edu>
2021-08-11 15:55:07 -0400

    Scoreboards displayed for each team

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Module dependencies
 
 const ejs = require("ejs");
+const fetch = require("node-fetch");
 const express = require("express");
 const path = require("path");
 const app = express();
@@ -14,16 +15,59 @@ app.set("views", path.join(__dirname, "/views"));
 
 app.use("/public", express.static("public"));
 
+
 // Routes
 
+// Homepage
 app.get("/", (req, res) => {
     res.render("index");
 })
 
+// Team scoreboards
 app.get("/teams/:tname", (req, res) => {
     var tname = req.params.tname;
-    res.render("teams", {teamn: tname});
+
+    const teamID = {
+        Hawks: 1,
+        Celtics: 2,
+        Nets: 3,
+        Hornets: 4,
+        Bulls: 5,
+        Cavs: 6,
+        Mavs: 7,
+        Nuggets: 8,
+        Detroit: 9,
+        Warriors: 10,
+        Rockets: 11,
+        Pacers: 12,
+        Clippers: 13,
+        Lakers: 14,
+        Grizzlies: 15,
+        Heat: 16,
+        Bucks: 17,
+        Timberwolves: 18,
+        Pelicans: 19,
+        Knicks: 20,
+        OKC: 21,
+        Magic: 22,
+        Philly: 23,
+        Suns: 24,
+        Trailblazers: 25, 
+        Kings: 26,
+        Spurs: 27,
+        Raptors: 28,
+        Jazz: 29,
+        Wizards: 30
+    };
+
+    var tid = teamID[tname];
+
+    fetch(`https://www.balldontlie.io/api/v1/games/?seasons[]=2018&team_ids[]=${tid}&per_page=82`)
+    .then(res => res.json())
+    .then(json => res.render("teams", {teamn: tname, games: json.data}))
+
 })
+
 
 // Server
 
